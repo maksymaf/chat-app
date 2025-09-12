6266b1db993198e0d1274b337461565c726b29b3
maksymaf <forbidden344@gmail.com>
2025-09-12 17:59:22 +0300

    chat messages exchange added

--- public/main.js
+++ public/main.js
@@ -1,2 +1,20 @@
 // io підключається до нашого сокет-серверу 
 const socket = io('http://localhost:3000');
+
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const messageData = document.getElementById('message').value;
+    document.getElementById('message').value = '';
+
+    socket.emit('clientMessage', messageData);
+});
+
+socket.on('clientMessage', (messageData) => {
+    if(messageData){
+        const ul = document.getElementById('messages')
+        const li = document.createElement('li');
+        li.textContent = `${messageData}`;
+        ul.append(li);
+        ul.scrollTop = ul.scrollHeight;
+    }
+}); 
--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-const server = app.listen(PORT);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
+    }
+});
+
+io.on('connect', (socket) => {
+    console.log(`Client has joined the server: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        console.log(`Client has left the server: ${socket.id}`);
+    });
+
+    socket.on('clientMessage', (messageData) => {
+        io.emit('clientMessage', messageData);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log('Server has successfuly started working.');
+})
